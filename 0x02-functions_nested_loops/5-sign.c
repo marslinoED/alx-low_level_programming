ff4eadccbb9bdf3f8535efb1c7a1156ede1a3e1c
marslinoED <lino.edward11@gmail.com>
2023-08-15 14:50:32 +0300

    alais

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - check for positive letter
+ * @n : integer to check
+ * Return:0 or 1 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
