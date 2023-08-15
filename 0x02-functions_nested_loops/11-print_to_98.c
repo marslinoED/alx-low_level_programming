679a81f09102d78c06102a3641c589cd22842ed8
marslinoED <lino.edward11@gmail.com>
2023-08-15 16:23:02 +0300

    alais

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
+ */
+
+void print_to_98(int n)
+{
+if (n > 98)
+	{
+	}
+	else if (n < 98)
+	{
+		{
+		while (n < 98)
+		{
+			_putchar(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+		_putchar('\n');
+		}
+		while (n < 98)
+		{
+			_putchar(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(98);
+		_putchar('\n');
+	}
+}
