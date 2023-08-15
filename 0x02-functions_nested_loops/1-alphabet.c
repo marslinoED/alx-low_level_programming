e9e003a773e57b28466486fffd6bdeef216827af
marslinoED <lino.edward11@gmail.com>
2023-08-15 13:28:41 +0300

    alais

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
 void print_alphabet(void)
-{	
-	char c='a';
-	while (c!=('z'-1))
+{
+	char c = 'a';
+
+	while (c != ('z' - 1))
 	{
-	putchar(c);
-	c++;
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 }
 
 
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 
-int main (void)
+int main(void)
 {
 	print_alphabet();
 	return (0);
