daf4c4cb4e93fdc6ba6d766ee9a585c4cb3fd4e3
marslinoED <lino.edward11@gmail.com>
2023-08-17 16:49:01 +0300

    alais

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@
 void print_line(int n)
 {
 
-        int i = 0;
+	int i = 0;
 
-        while (i < n && n > 0)
-        {
-                _putchar('_');
-                i++;
-        }
-        _putchar('\n');
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
 
 }
