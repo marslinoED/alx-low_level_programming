8fca249dbe00d073f57568c552d96aa1dc092ea3
marslinoED <lino.edward11@gmail.com>
2023-08-17 16:03:34 +0300

    alais

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * void print_most_numbers - ll
+ * print_most_numbers - ll
  * Return: 0
  */
 
 void print_most_numbers(void)
 {
 	int i = 0;
+
 	while (i < 10)
 	{
 		if ((i != 2) && (i != 4))
