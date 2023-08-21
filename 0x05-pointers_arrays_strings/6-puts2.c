ccc15f6a65b3221664f2635ffbe835b424647019
marslinoED <lino.edward11@gmail.com>
2023-08-21 18:43:50 +0300

    alais

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * puts2 - ll
+ * @str: ll
+ * Return: ll
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	
+	while (s[i])
+		i++;
+
+	for (int j = 0; j < i; j+=2)
+		{
+			_putchar(s[i]);
+		}
+}
