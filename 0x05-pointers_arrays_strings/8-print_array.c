7b3b8d3f2e46cd4a0665d1c7df0ec4d793a06311
marslinoED <lino.edward11@gmail.com>
2023-08-21 19:18:44 +0300

    alais

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - ll
+ * @a: ll
+ * @n: ll
+ * Return: ll
+ */
+
+void print_array(int *a, int n)
+{
+int i = 0;
+while (n != (i - 1)
+{
+	printf ("%d, ", a[i]);
+	i++;
+}
+printf("%d\n", a[i]);
+
+}
