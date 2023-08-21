785c003128ef9d13ec8220b1160aa9a8f067c41d
marslinoED <lino.edward11@gmail.com>
2023-08-21 18:37:26 +0300

    alais

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_rev - ll
- * @s: ll
- * Return: ll
- */
+ * print_rev - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
 
 void print_rev(char *s)
 {
