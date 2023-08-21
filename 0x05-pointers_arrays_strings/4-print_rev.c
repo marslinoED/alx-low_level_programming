62ad7b0031494603471852a96fd08dda5654ed5f
marslinoED <lino.edward11@gmail.com>
2023-08-21 18:13:55 +0300

    alais

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - ll
+ * @s: ll
+ * Return: ll
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		{
+			i++;
+		}
+
+	while (i--)
+		{
+			_putchar(s[i]);
+		}
+
+	_putchar('\n');
+
+}
