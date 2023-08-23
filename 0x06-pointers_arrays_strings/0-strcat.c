6cf9d9aea28ca8bf47f5dc577d543ba60e9520f7
marslinoED <lino.edward11@gmail.com>
2023-08-23 21:39:32 +0300

    alais

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,37 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * _strcat - ll
- * @dest: ll
- * @src: ll
- * Return: ll
- */
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, a = 0, z = 0, x = 0, y = 0;
-	char *new = (char *)malloc(i + a + 1);
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[a] != '\0')
-		a++;
-
-	while (i--)
-	{
-		new[z] = dest[x];
-		z++;
-		x++;
-	}
+do {
 	a++;
-	while (a--)
-	{
-		new[z] = src[y];
-		z++;
-		y++;
-	}
-	new[z] = '\n';
-	return (new);
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
+
