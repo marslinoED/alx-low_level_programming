8cf488fdd7ab13c2bdd0d904ffd14441c2c1a9f9
marslinoED <lino.edward11@gmail.com>
2023-08-28 14:31:19 +0300

    alais

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - ll
+ * @dest: ll
+ * @src: ll
+ * @n: ll
+ * Return: ll
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
