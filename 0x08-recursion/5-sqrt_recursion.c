9b371101ca9e2a0bf7a7d749ad0d0d4e3ed16508
marslinoED <lino.edward11@gmail.com>
2023-08-30 17:27:58 +0300

    alais

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - ll
+ * @n: ll
+ * Return: ll
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt(x));
+}
