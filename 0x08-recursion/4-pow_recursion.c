d9d18e79d08a47873471a8c513254f10e0094cd1
marslinoED <lino.edward11@gmail.com>
2023-08-30 16:40:32 +0300

    alais

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - ll
+ * @x: ll
+ * @y: ll
+ * Return: ll
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
