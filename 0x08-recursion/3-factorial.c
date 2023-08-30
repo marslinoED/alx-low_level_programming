d4635a430bc64cae02dcbcbb80d2519cbd5285ab
marslinoED <lino.edward11@gmail.com>
2023-08-30 16:34:05 +0300

    alais

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - ll
+ * @n: ll
+ * Return: fac
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	if (n == 1)
+		return n;
+	n = n * fac(n -1);
+}
