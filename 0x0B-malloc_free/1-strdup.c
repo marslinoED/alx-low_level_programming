89ef252dac2ddbce66be5a60210937da63b93107
marslinoED <lino.edward11@gmail.com>
2023-09-04 18:14:57 +0300

    alais

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - ll
+ * @str: ll
+ * Return: ll
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *c;
+
+	while (str[size] != '\0')
+		size++;
+
+	if (size == 0)
+		return (NULL);
+
+	c = (char *) malloc(sizeof(c) * size);
+
+	if (c == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		c[i] = str[i];
+
+	return (c);
+}
