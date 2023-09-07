c68bb55053c85fc33be7bbdf465f4facd826167e
marslinoED <lino.edward11@gmail.com>
2023-09-07 16:32:12 +0300

    alais

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,42 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include <Stdlib.h>
 
 /**
- * _memset - copy char
- * @s: string
- * @n: bytes
- * @min: ll
- * @max: lll
- * Return: string
+ * array_range - creates an array of integers
+ * @min: minimum
+ * @max: maximum
+ * Return: array
  */
-
-int *_memset(int *s, int min, int max, unsigned int n)
-{
-	unsigned int i, j = 0;
-
-	for (i = min; i <= max; i++)
-	{
-		s[j] = i;
-		j++;
-	}
-	return (s);
-}
-
-/**
- * array_range - ll
- * @min: ll
- * @max: ll
- * Return: ll
- */
-
 int *array_range(int min, int max)
 {
-	int size, *arr;
+	int *arr, i = 0, t = min;
 
 	if (min > max)
-		return (NULL);
-	size = (max - min) + 1;
-	arr = (int *) malloc(sizeof(int) * size);
-	return (_memset(arr, min, max, size));
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
+	return (arr);
 }
+
