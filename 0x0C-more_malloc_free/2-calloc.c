bc3eba1d7accf710f434aeb59e9cbf92c4c2dfa8
marslinoED <lino.edward11@gmail.com>
2023-09-07 16:13:01 +0300

    alais

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
 
 /**
  * _calloc - allocates memory for an array using malloc
@@ -14,9 +30,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = calloc(nmemb * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
+	_memset(p, 0, (nmemb * size));
 	return (p);
 }
+
