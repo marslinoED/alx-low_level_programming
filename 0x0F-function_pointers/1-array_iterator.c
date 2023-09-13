9267e845f2bc3f2a841a068b69fd1c6e1449cc94
marslinoED <lino.edward11@gmail.com>
2023-09-13 14:59:39 +0300

    alais

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes funct
+ * @array: array of elements
+ * @size: array's size
+ * @action: pointer to function
+ * Return: No
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (size > 0)
+		action(array);
+}
