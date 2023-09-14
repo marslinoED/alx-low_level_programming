385b5b3ea7f0fd1e97654cc58c0bfcd4290efcaf
marslinoED <lino.edward11@gmail.com>
2023-09-14 15:33:49 +0300

    alais

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, unsigned int));
+		printf("%i", va_arg(args, unsigned int));
 		if (i != n - 1)
 			printf("%s", separator);
 	}
