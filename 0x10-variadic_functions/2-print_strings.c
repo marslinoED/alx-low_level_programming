83292eda80da4e589d696bb566833a6ed1c0e853
marslinoED <lino.edward11@gmail.com>
2023-09-14 16:27:41 +0300

    alais

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,12 +12,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
-
+	char *c;
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-        printf("%s", va_arg(list, char*));
+		c = va_arg(list, char*);
+		if (c != null)
+        		printf("%s", c);
+		else
+			printf("nill");
         if (i != n - 1 && separator != NULL)
                 printf("%s", separator);
 	}
